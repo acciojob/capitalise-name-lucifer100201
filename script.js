60bd83dd994f5fe1da975be21d6b51682ce6344b
lucifer100201 <99162740+lucifer100201@users.noreply.github.com>
2024-03-19 15:07:29 -0600

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-let input = document.querySelectorById("fname")
+let inputValue = document.querySelector("input");
+
+inputValue.addEventListener("keydown", function(event){
+	if(event.key === 'Enter'){
+		capitaliseName(event);
+	}
+})
+
+function capitaliseName(event) {
+    let nameValue = event.target.value;
+    let res = "";
+    for (let i = 0; i < nameValue.length; i++) {
+        let value = nameValue.charCodeAt(i);
+        if (value >= 97 && value <= 122) {
+            value -= 32;
+            res += String.fromCharCode(value);
+        } else {
+            res += String.fromCharCode(value);
+        }
+    }
+	console.log(res);
+    event.target.value = res;
+}
